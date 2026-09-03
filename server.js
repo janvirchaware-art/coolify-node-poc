112b676716d05a968e725765655a7a73ceb14264
Janvi Chaware <janvichaware29@gmail.com>
2026-09-03 16:17:54 +0530

    Initial Node.js Coolify POC

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node.js - Coolify POC");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "ok",
+        application: "node-poc"
+    });
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Node application running on port ${PORT}`);
+});
